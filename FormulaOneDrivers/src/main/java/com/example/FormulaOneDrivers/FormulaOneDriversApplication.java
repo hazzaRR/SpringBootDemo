c44463f4fdd2985335dc45b25b6040c5f458cb90
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-07-10 21:18:13 +0100

    added spring boot template

---
+++ FormulaOneDrivers/src/main/java/com/example/FormulaOneDrivers/FormulaOneDriversApplication.java
@@ -0,0 +1,13 @@
+package com.example.FormulaOneDrivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaOneDriversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaOneDriversApplication.class, args);
+	}
+
+}
---
+++ FormulaOneDrivers/src/test/java/com/example/FormulaOneDrivers/FormulaOneDriversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FormulaOneDrivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaOneDriversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
